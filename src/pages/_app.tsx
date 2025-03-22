63c54463c594c1f463dc78d62eb9679a7c6f3d33
adriano25klein@gmail.com <you@example.com>
2025-03-22 11:54:21 -0300

    Otimizando as fontes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
-import type { AppProps } from "next/app";
-import { GlobalStyle } from "../styles/globalStyles";
-import { ThemeProvider } from "styled-components";
-import { theme } from "../styles/theme";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Roboto } from "next/font/google"
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Roboto } from 'next/font/google';
 
 const roboto = Roboto({
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <SpeedInsights />
+      <GlobalStyle />
       <div className={roboto.className}>
-        <SpeedInsights />
-        <GlobalStyle />
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
