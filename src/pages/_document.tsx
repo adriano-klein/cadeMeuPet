cec5986fe2c459d5bedd42333558f135afebd21e
adriano25klein@gmail.com <you@example.com>
2025-03-23 10:27:02 -0300

    fix: Correct Head component usage and set page title
    - Moved title definition inside the Head component.
    - Set lang attribute on the Html tag to 'pt-BR'.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head title="Cadê meu Pet" lang="pt-BR"/>
         <body>
           <Main />
           <NextScript />
